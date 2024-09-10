e038a5a0ed121921bf98e372b571d4d25c975755
ethan <ethan.hugerot@hotmail.fr>
2024-09-10 14:18:57 +0200

    Récupérez les paramètres en PHP

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- footer.php -->
+<!-- footer.php -->
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2024 Copyright:
+    <a href="https://github.com/ethan-hgt">Ethan</a>
+  </div>
+</footer>
