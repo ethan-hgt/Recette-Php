3843f02a0743c1bb82659fb335686995e35abb9f
ethan <ethan.hugerot@hotmail.fr>
2024-09-10 14:33:18 +0200

    Testez la présence d'un paramètre

--- submit_contact.php
+++ submit_contact.php
@@ -1,45 +1,45 @@
-<!-- index.php -->
+<!-- submit_contact.php -->
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site de recettes - Page d'accueil</title>
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-        rel="stylesheet"
-    >
+    <title>Site de recettes - Récapitulatif de Contact</title>
+    <link 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+    rel="stylesheet">
 </head>
 <body class="d-flex flex-column min-vh-100">
     <div class="container">
 
     <!-- inclusion de l'entête du site -->
     <?php include_once('header.php'); ?>
-    
-    <h1>Site de recettes</h1>
-    
-    <!-- inclusion des variables et fonctions -->
+
+    <h1>Récapitulatif de Contact</h1>
+
     <?php
-        include_once('variables.php');
-        include_once('functions.php');
+    if (!isset($_GET['email']) || !isset($_GET['message'])) {
+        echo '<h1>Il faut un email et un message pour soumettre le formulaire.</h1>';
+        return; // Arrêter l'exécution de PHP
+    }
+
+    $email = htmlspecialchars($_GET['email']);
+    $message = htmlspecialchars($_GET['message']);
     ?>
 
-    <div>
-        <h2>Message bien reçu !</h2>
-        
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Rappel de vos informations</h5>
-                <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?></p>
-                <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?></p>
-            </div>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Rappel de vos informations</h5>
+            <p class="card-text"><b>Email</b> : <?php echo $email; ?></p>
+            <p class="card-text"><b>Message</b> : <?php echo $message; ?></p>
         </div>
     </div>
-    
-    <!-- inclusion du bas de page du site -->
-    <?php include_once('footer.php'); ?>
-
+    <br>
     </div>
+
+    <!-- inclusion du bas de page -->
+    <?php include_once('footer.php'); ?>
+    
 </body>
 </html>
